44bd0e54681263214356752c0e24cfe727a887db
laaptu <laaptu@laaptus-MacBook-Pro.local>
2016-03-21 10:35:50 +0545

    Adding Fragment State adapter

--- app/src/main/java/com/lftechnology/hamropay/adapter/ContactsPagerAdapter.java
+++ app/src/main/java/com/lftechnology/hamropay/adapter/ContactsPagerAdapter.java
@@ -3,19 +3,16 @@ package com.lftechnology.hamropay.adapter;
 import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.lftechnology.hamropay.fragment.AllContactsFragment;
 import com.lftechnology.hamropay.fragment.FrequentsFragment;
 import com.lftechnology.hamropay.fragment.ServicesFragment;
-import com.lftechnology.hamropay.fragment.base.ContactBaseFragment;
-
-import java.util.List;
 
 /**
  * Created by rajesh on 2/16/16.
  */
-public class ContactsPagerAdapter extends FragmentPagerAdapter {
+public class ContactsPagerAdapter extends FragmentStatePagerAdapter {
     final int PAGE_COUNT = 3;
     private String tabTitles[] = new String[]{"FREQUENTS", "ALL CONTACTS", "SERVICES"};
     private Context context;
@@ -34,17 +31,17 @@ public class ContactsPagerAdapter extends FragmentPagerAdapter {
     }
 
     public void removeAllFragments() {
-        try {
-            if (fm != null) {
-                List<Fragment> lists = fm.getFragments();
-                for (Fragment fragment : lists) {
-                    if (fragment instanceof ContactBaseFragment)
-                        fm.beginTransaction().remove(fragment).commit();
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//        try {
+//            if (fm != null) {
+//                List<Fragment> lists = fm.getFragments();
+//                for (Fragment fragment : lists) {
+//                    if (fragment instanceof ContactBaseFragment)
+//                        fm.beginTransaction().remove(fragment).commit();
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
 
     }
 
--- app/src/main/java/com/lftechnology/hamropay/adapter/DashboardPagerAdapter.java
+++ app/src/main/java/com/lftechnology/hamropay/adapter/DashboardPagerAdapter.java
@@ -2,7 +2,7 @@ package com.lftechnology.hamropay.adapter;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 
 import com.lftechnology.hamropay.fragment.ContactsFragment;
 import com.lftechnology.hamropay.fragment.RecentFragment;
@@ -13,7 +13,7 @@ import java.util.ArrayList;
 /**
  * Created by rajesh on 2/15/16.
  */
-public class DashboardPagerAdapter extends FragmentPagerAdapter {
+public class DashboardPagerAdapter extends FragmentStatePagerAdapter {
 
     private Fragment fragment;
     private ArrayList<TabData> tabList;
